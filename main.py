12dcf69a9c53a3bf9da91af3f0277822fda6998e
Antti Rasinen <antti.rasinen@codento.com>
2012-10-20 23:36:30 +0300

    Print out details

--- main.py
+++ main.py
@@ -1,8 +1,44 @@
 import requests
 import json
+import sys
 
-r = requests.get('http://posterous.com/api/2/sites/arsatiki/posts/public')
-print "Found", len(r.json[0]), "items"
+if len(sys.argv) < 2:
+    sys.exit()
+
+vals = dict(site=sys.argv[1])
+
+
+r = requests.get('http://posterous.com/api/2/sites/%(site)s/posts/public' % vals)
+print "Found", len(r.json), "items"
 print "Keys:"
-for k in r.json[0]:
+for k in sorted(r.json[0]):
     print "-", k
+
+
+interesting =     '''
+display_date
+short_url
+id
+title
+media
+slug
+full_url
+tags
+site
+user
+replies_count
+number_of_comments
+comments_count
+'''.split()
+# bodies:
+# body_full
+# body_excerpt
+# body_cleaned
+# body_html
+
+for p in r.json:
+    print "Post", p['title']
+    for k in interesting:
+        print k, p[k]
+    print
+
