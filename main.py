c85e5091502e13e96626f64685000892c3e0b2c8
Antti Rasinen <antti.rasinen@codento.com>
2012-10-21 12:58:54 +0300

    Fetch posts from all pages

--- main.py
+++ main.py
@@ -9,6 +9,20 @@ def fix_date(s, tz):
         return None
     return parser.parse(s).astimezone(tz)
 
+def get_posts(site):
+    url = 'http://posterous.com/api/2/sites/%s/posts/public' % site
+    p = []
+    page = 1
+    while True:
+        params = {'page': page}
+        r = requests.get(url, params=params)
+        if not r.json:
+            break
+        p.extend(r.json)
+        page += 1
+    return p
+    
+
 if len(sys.argv) < 3:
     sys.exit()
 
@@ -19,9 +33,9 @@ if tz is None:
 
 vals = dict(site=sys.argv[1])
 
+posts = get_posts(sys.argv[1])
 
-r = requests.get('http://posterous.com/api/2/sites/%(site)s/posts/public' % vals)
-print "Found", len(r.json), "items"
+print "Found", len(posts), "items"
 
 interesting =     '''
 media
@@ -37,7 +51,7 @@ comments_count
 # body_cleaned
 # body_html
 
-for p in r.json:
+for p in posts:
     # Fix date
     other = {}
     if 'display_date' in p:
