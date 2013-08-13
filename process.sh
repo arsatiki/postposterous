39f8e423a682eb422a276b3dba6438eb086ccf50
Antti Rasinen <antti.rasinen@codento.com>
2013-08-13 10:55:13 +0300

    Copy images to statics

--- process.sh
+++ process.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 TARGET=content/$1
+STATIC=static/$1
 mkdir -p $TARGET
+mkdir -p $STATIC
+
 
 for article in originals/$1/*; do
+	# TODO: comments.json
+	cp $article/*.{jpg,jpeg,png} $STATIC/
 	(
 		echo ---
 		cat $article/metadata.yaml
